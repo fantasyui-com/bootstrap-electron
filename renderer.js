4d80f6f7b1415a9ab53c198675b8fb626e93659e
Captain Fantasy <captain@fantasyui.com>
2017-08-09 10:21:43 -0400

    Add Vue, jQuery and EventEmitter integration.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,60 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const pkg = require(__dirname + '/package.json')
+const EventEmitter = require('events');
+const fs = require('fs');
+const path = require('path');
+
+class MyEmitter extends EventEmitter {}
+const ee = new MyEmitter();
+
+
+
+
+/* * *
+  BODY DROP INIT
+* * */
+
+document.ondragover = document.ondrop = (ev) => {
+  ev.preventDefault();
+}
+document.body.ondrop = (ev) => {
+  ev.preventDefault()
+  const file = ev.dataTransfer.files[0].path;
+  ee.emit('document-drop', {file})
+}
+
+
+
+/* * *
+  VUE COMPONENT EXAMPLE
+* * */
+
+var appTitle = new Vue({
+  el: '#app-title',
+  data: pkg
+});
+
+
+
+
+/* * *
+  JQUERY EXAMPLE
+* * */
+
+$(function(){
+  $('title').text(pkg.name);
+});
+
+
+
+
+/* * *
+  INTERNAL API EXAMPLE
+* * */
+
+ee.on('document-drop', (data) => {
+  alert('Dropped ' + data.file);
+});
